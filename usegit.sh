acd5a0b2a64d76aec5f786015dfdbffd11912883
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-01-08 01:58:37 +0100

    Update usegit.sh

--- usegit.sh
+++ usegit.sh
@@ -2,7 +2,7 @@
 [[ -f "./.git/.usegit.name" ]] && target=".git_"$(<".git/.usegit.name") && rm -rf $target && mv ".git" $target && echo $target saved
 [[ -d "./.git" ]] && read -p "Backup .git to .git_"$1" (Y/n)? " -n 1 -r && [[ $REPLY =~ ^[Nn]$ ]] && echo "othing changed" && exit 1
 [[ -d "./.git" ]] && echo $1 > ./.git/.usegit.name && mv -f "./.git/" "./.git_"$1"/" && echo "backed up .git to .git_"$1 && exit 0
-[[ -d "./.git" ]] && echo error: couldnt backup to .git_"$1 && exit 1
+[[ -d "./.git" ]] && echo "error: couldnt backup to .git_"$1 && exit 1
 
 rm -rf ./.git/*
 [[ -d "./.git_"$1 ]] || echo "error: .git_"$1" not found"
